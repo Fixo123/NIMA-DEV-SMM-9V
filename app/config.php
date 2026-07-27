4766d6069896ebc6e1cf7c9705ce06c55c1f6f6d
FIXO DEV <videodd42@gmail.com>
2026-07-27 11:50:34 +0530

    🔒 𝗡𝗜𝗠𝗔 𝗦𝗠𝗠 🔒 app/config.php

---
+++ app/config.php
@@ -0,0 +1,23 @@
+<?php
+define('PATH', realpath('.'));
+define('SUBFOLDER', false);
+define('URL', 'https://chocolate-woodpecker-370173.hostingersite.com' );
+define('STYLESHEETS_URL', '//chocolate-woodpecker-370173.hostingersite.com');
+date_default_timezone_set('Asia/Karachi');
+
+
+ ini_set("display_errors","1");
+error_reporting(E_ALL);  
+
+error_reporting(0);
+return [
+  'db' => [
+    'name'    =>  'u435768449_hdsmm' ,
+    'host'    =>  'localhost',
+    'user'    =>  'u435768449_hdsmm' ,
+    'pass'    =>  '6>xkywYk' ,
+    'charset' =>  'utf8mb4' 
+  ]
+];
+
+?>
